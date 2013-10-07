5c05fa9acfd53473aac4c9b2042491e253131e25
Jonathan Williamson <jon@pimoroni.com>
2013-10-07 10:35:15 +0000

    Added header to example

--- example.py
+++ example.py
@@ -1,3 +1,11 @@
+# ADXL345 Python example 
+#
+# author:  Jonathan Williamson
+# license: BSD, see LICENSE.txt included in this package
+# 
+# This is an example to show you how to use our ADXL345 Python library
+# http://shop.pimoroni.com/products/adafruit-triple-axis-accelerometer
+
 from adxl345 import ADXL345
   
 adxl345 = ADXL345()
