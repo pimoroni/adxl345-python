d0dcb26c4878c126a4ce5ea17ce2727786e1cb0c
Jonathan Williamson <jon@pimoroni.com>
2013-10-07 10:34:28 +0000

    Added header

--- adxl345.py
+++ adxl345.py
@@ -1,3 +1,12 @@
+# ADXL345 Python library for Raspberry Pi 
+#
+# author:  Jonathan Williamson
+# license: BSD, see LICENSE.txt included in this package
+# 
+# This is a Raspberry Pi Python implementation to help you get started with
+# the Adafruit Triple Axis ADXL345 breakout board:
+# http://shop.pimoroni.com/products/adafruit-triple-axis-accelerometer
+
 import smbus
 from time import sleep
 
